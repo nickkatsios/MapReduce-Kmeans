916d77e62cb07842d1fd00742e3ebc0dc97f47cc
NickKatsios <t8200071@aueb.gr>
2023-04-18 18:16:46 +0300

    Adjust files from 1D to 2D. See TODOs.

--- src/main/java/gr/aueb/dmst/nickkatsios/Kmeans.java
+++ src/main/java/gr/aueb/dmst/nickkatsios/Kmeans.java
@@ -1,5 +1,6 @@
 package gr.aueb.dmst.nickkatsios;
 
+import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.util.*;
 import java.io.*;
@@ -11,6 +12,12 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.mapred.Reducer;
 
+// TODO: replace output.collect with context.write
+// TODO: replace the input and output classes since we are dealing with (x,y) coordinates
+//  represented as Point2D.Double objects and not single double values
+// TODO: Simplify initial argument files
+// TODO: Decide sorting of Point2D old and new centers to match them in the comparison: https://stackoverflow.com/questions/29030348/collections-sort-doesnt-work-on-listpoint2d-double
+
 @SuppressWarnings("deprecation")
 public class Kmeans {
     public static String OUT = "outfile";
@@ -19,8 +26,9 @@ public class Kmeans {
     public static String OUTPUT_FILE_NAME = "/part-00000";
     public static String DATA_FILE_NAME = "/data.txt";
     public static String JOB_NAME = "KMeans";
-    public static String SPLITTER = "\t| ";
-    public static List<Double> mCenters = new ArrayList<Double>();
+
+    public static double CONVERGENCE_THRESHOLD = 0.01;
+    public static List<Point2D.Double> mCenters = new ArrayList<Point2D.Double>();
 
     /*
      * In Mapper class we are overriding configure function. In this we are
@@ -29,6 +37,10 @@ public class Kmeans {
      */
     public static class Map extends MapReduceBase implements
             Mapper<LongWritable, Text, DoubleWritable, DoubleWritable> {
+
+        // configure runs once before the whole mapping process and for each job
+        // clears the old centroids and substitutes them with the new ones , located in
+        // the file in the distributed cache
         @Override
         public void configure(JobConf job) {
             try {
@@ -44,8 +56,9 @@ public class Kmeans {
                         // Read the file split by the splitter and store it in
                         // the list
                         while ((line = cacheReader.readLine()) != null) {
-                            String[] temp = line.split(SPLITTER);
-                            mCenters.add(Double.parseDouble(temp[0]));
+                            String[] parts = line.split(" ");
+                            Point2D.Double center = new Point2D.Double(Double.parseDouble(parts[0]) , Double.parseDouble(parts[1]));
+                            mCenters.add(center);
                         }
                     } finally {
                         cacheReader.close();
@@ -64,51 +77,56 @@ public class Kmeans {
         public void map(LongWritable key, Text value,
                         OutputCollector<DoubleWritable, DoubleWritable> output,
                         Reporter reporter) throws IOException {
+            // read the values from the data file
             String line = value.toString();
-            double point = Double.parseDouble(line);
-            double min1, min2 = Double.MAX_VALUE, nearest_center = mCenters
-                    .get(0);
+            String[] parts = line.split(" ");
+            Point2D.Double point = new Point2D.Double(Double.parseDouble(parts[0]) , Double.parseDouble(parts[1]));
+            double minDist = Double.MAX_VALUE;
+            Point2D.Double nearest_center = mCenters.get(0);
             // Find the minimum center from a point
-            for (double c : mCenters) {
-                min1 = c - point;
-                if (Math.abs(min1) < Math.abs(min2)) {
-                    nearest_center = c;
-                    min2 = min1;
+            for (Point2D.Double center : mCenters) {
+                double dist = Point2D.distance(point.getX() , point.getY() , center.getX() , center.getY());
+                if (dist < minDist) {
+                    nearest_center = center;
+                    minDist = dist;
                 }
             }
             // Emit the nearest center and the point
-            output.collect(new DoubleWritable(nearest_center),
-                    new DoubleWritable(point));
+            //output.collect(new DoubleWritable(nearest_center),
+            //        new DoubleWritable(point));
         }
     }
 
     public static class Reduce extends MapReduceBase implements
-            Reducer<DoubleWritable, DoubleWritable, DoubleWritable, Text> {
+            Reducer<DoubleWritable, Text, DoubleWritable, Text> {
 
         /*
          * Reduce function will emit all the points to that center and calculate
          * the next center for these points
          */
+        // reduce runs once for every key in the mapped key-value pairs
         @Override
-        public void reduce(DoubleWritable key, Iterator<DoubleWritable> values,
+        public void reduce(DoubleWritable key, Iterator<Text> values,
                            OutputCollector<DoubleWritable, Text> output, Reporter reporter)
                 throws IOException {
-            double newCenter;
-            double sum = 0;
-            int no_elements = 0;
-            String points = "";
-            while (values.hasNext()) {
-                double d = values.next().get();
-                points = points + " " + Double.toString(d);
-                sum = sum + d;
-                ++no_elements;
-            }
 
-            // We have new center now
-            newCenter = sum / no_elements;
+            double sumX = 0.0;
+            double sumY = 0.0;
+            int count = 0;
+            for (Iterator<Text> it = values; it.hasNext(); ) {
+                Text value = it.next();
+                String[] parts = value.toString().split(" ");
+                if (parts.length == 2) {
+                    Point2D.Double point = new Point2D.Double(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
+                    sumX += point.getX();
+                    sumY += point.getY();
+                    count++;
+                }
+            }
+            Point2D.Double newCenter = new Point2D.Double(sumX / count, sumY / count);
 
             // Emit new center and point
-            output.collect(new DoubleWritable(newCenter), new Text(points));
+            //output.collect(new DoubleWritable(newCenter), new Text(points));
         }
     }
 
@@ -158,12 +176,12 @@ public class Kmeans {
             FileSystem fs = FileSystem.get(new Configuration());
             BufferedReader br = new BufferedReader(new InputStreamReader(
                     fs.open(ofile)));
-            List<Double> centers_next = new ArrayList<Double>();
+            List<Point2D.Double> centers_next = new ArrayList<Point2D.Double>();
             String line = br.readLine();
             while (line != null) {
-                String[] sp = line.split("\t| ");
-                double c = Double.parseDouble(sp[0]);
-                centers_next.add(c);
+                String[] parts = line.split(" ");
+                Point2D.Double center = new Point2D.Double(Double.parseDouble(parts[0]) , Double.parseDouble(parts[1]));
+                centers_next.add(center);
                 line = br.readLine();
             }
             br.close();
@@ -178,25 +196,29 @@ public class Kmeans {
             FileSystem fs1 = FileSystem.get(new Configuration());
             BufferedReader br1 = new BufferedReader(new InputStreamReader(
                     fs1.open(prevfile)));
-            List<Double> centers_prev = new ArrayList<Double>();
-            String l = br1.readLine();
-            while (l != null) {
-                String[] sp1 = l.split(SPLITTER);
-                double d = Double.parseDouble(sp1[0]);
-                centers_prev.add(d);
-                l = br1.readLine();
+            List<Point2D.Double> centers_prev = new ArrayList<Point2D.Double>();
+            line = br1.readLine();
+            while (line != null) {
+                String[] parts = line.split(" ");
+                Point2D.Double center = new Point2D.Double(Double.parseDouble(parts[0]) , Double.parseDouble(parts[1]));
+                centers_prev.add(center);
+                line = br1.readLine();
             }
             br1.close();
 
-            // Sort the old centroid and new centroid and check for convergence
-            // condition
+            // Sort the old centroid and new centroid and check for convergence condition
+            //we sort them to check if they are close enough one by one
             Collections.sort(centers_next);
             Collections.sort(centers_prev);
 
-            Iterator<Double> it = centers_prev.iterator();
-            for (double d : centers_next) {
-                double temp = it.next();
-                if (Math.abs(temp - d) <= 0.1) {
+            Iterator<Point2D.Double> it = centers_prev.iterator();
+
+            for (Point2D.Double old_center : centers_next) {
+                Point2D.Double new_center = it.next();
+                // got the 2 centers
+                // now check if they are close enough
+                double dist = Point2D.distance(old_center.getX() , old_center.getY() , new_center.getX() , new_center.getY());
+                if (dist <= CONVERGENCE_THRESHOLD) {
                     isdone = true;
                 } else {
                     isdone = false;
